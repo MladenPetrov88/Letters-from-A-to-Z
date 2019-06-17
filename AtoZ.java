9ffde0f8e3123970a995219f7d13727719bf2c24
MladenPetrov88 <50301670+MladenPetrov88@users.noreply.github.com>
2019-06-17 10:01:22 +0300

    Add files via upload

---
+++ AtoZ.java
@@ -0,0 +1,8 @@
+public class AtoZ {
+    public static void main(String[] args) {
+
+        for (char letter = 'a'; letter <= 'z'; letter++) {
+            System.out.println(letter);
+        }
+    }
+}
